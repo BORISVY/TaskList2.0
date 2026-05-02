8692c9cb695f8c36b627da4be1e1bc7be0a72a07
João Pedro Bergamini <joaopedrobergamini16062000@gmail.com>
2026-05-02 09:44:13 -0300

    New Functions From User Control (CRUD USER)

--- services/services.py
+++ services/services.py
@@ -13,3 +13,17 @@ def connection():
         raise e
     finally:
         conn.close()
+
+class Services():
+    def new_user(self, username, email, password):
+        with connection() as cursor:
+            cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+                           (username, email, password))
+            
+    def edit_username(self, user_id, new_username):
+        with connection() as cursor:
+            cursor.execute("UPDATE users SET username = ? WHERE id = ?", (new_username, user_id))
+    
+    def delete_user(self, user_id):
+        with connection() as cursor:
+            cursor.execute("DEFELE FROM users WHERE id = ?", (user_id))
