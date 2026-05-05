7ac596c73f61d5c6cc1330ee8990954ba513e5bf
João Pedro Bergamini <joaopedrobergamini16062000@gmail.com>
2026-05-05 05:10:48 -0300

    Function UPDATE for Tasks (Complete CRUD)

--- services/services.py
+++ services/services.py
@@ -66,6 +66,16 @@ class UserTaskService:
         with connection() as cursor:
             cursor.execute("SELECT * FROM tasks")
             return cursor.fetchall()
+        
+    def edit_title_task(self, task_id, new_title):
+        with connection() as cursor:
+            cursor.execute("UPDATE tasks SET title = ? WHERE id = ?", (new_title, task_id))
+            return cursor.rowcount
+        
+    def edit_desc_task(self, task_id, new_description):
+        with connection() as cursor:
+            cursor.execute("UPDATE tasks SET description = ? WHERE id = ?", (new_description, task_id))
+            return cursor.rowcount
 
     def delete_task(self, task_id):
         with connection() as cursor:
