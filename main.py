e0ae9ff7c33638b28c2b8b72e7bd51882c76e0ff
João Pedro Bergamini <joaopedrobergamini16062000@gmail.com>
2026-05-07 06:06:48 -0300

    Created Routes in FastApi from Users

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from services import UserTaskService
+from srv.services import UserTaskService
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
@@ -10,6 +10,9 @@ class UserCreate(BaseModel):
     username: str
     email: str
     password: str
+
+class UserUpdate(BaseModel):
+    new_username: str
 
 class TaskCreate(BaseModel):
     title: str
@@ -35,7 +38,27 @@ def login(data: LoginRequest):
 
 @app.post("/users")
 def create_user(user: UserCreate):
-    service.new_user(user.title, user.email, user.password)
+    service.new_user(user.username, user.email, user.password)
     return {"message": "Usuário criado com sucesso"}
 
+@app.put("/users/{user_id}")
+def update_username(user_id, user: UserUpdate):
+    service.edit_username(user_id, user.new_username)
+    return {"message": "Nome de usuário atualizado com sucesso"}
 
+@app.delete("/users/{user_id}")
+def delete_user(user_id):
+    service.delete_user(user_id)
+    return {"message": "Usuário deletado com sucesso"}
+
+@app.get("/users/{user_id}")
+def get_user(user_id):
+    user = service.get_user(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    return {"message": "Usuário localizado com sucesso",
+            "user": {
+            "id": user[0],
+            "username": user[1],
+            "email": user[2]
+        }}
